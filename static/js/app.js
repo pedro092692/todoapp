e1b64a0168450d0ec5505d88b93c0b4118a59dad
pedro092692 <pedro_092692@hotmail.com>
2024-08-17 17:39:02 -0400

    update render template on first task

--- static/js/app.js
+++ static/js/app.js
@@ -11,17 +11,29 @@
 function show_add_form(){
     const add_button = document.getElementById('add-button');
     const form = document.getElementById('add-form');
+
+
+    addEventListener('click', (event)=>{
+        if(!event.target.contains(add_button) && !event.target.contains(form.children[0]) && form.children[0].value == ''){
+            hide_form();
+        }
+    })
+
     add_button.addEventListener('click', ()=>{
        add_button.style.display = 'none';
-       form.style.display = 'block';
+       form.style.display = 'flex';
        form.children[0].value = '';
        form.children[0].focus();
     })
 
-    form.addEventListener('submit', ()=>{
+    form.addEventListener('submit', hide_form)
+
+
+
+    function hide_form(){
         add_button.style.display = 'block';
         form.style.display = 'none';
-    })
+    }
 }
 
 show_add_form();
